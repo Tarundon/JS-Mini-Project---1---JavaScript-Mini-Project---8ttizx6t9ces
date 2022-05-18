0822abeddf11f8da5b9826a65f72bb821e3e7334
Tarundon <tdongre243@gamil.com>
2022-05-18 23:09:17 +0530

    first commit

--- App.js
+++ App.js
@@ -1,1 +1,18 @@
+function validate(){
+    //event.preventDefault();
+    //console.log("entered")
+    let userid = document.getElementById("email").value
+    let password = document.getElementById("password").value
+    
+    let user_records=new Array();
+   user_records=JSON.parse(localStorage.getItem("users"))?JSON.parse(localStorage.getItem("users")):[]
+   if(user_records.some((v)=>{return v.email==userid && v.password==password}))
+  {
+    alert("Logged in successfully!");
+  }
+  else
+{
+  alert('Login Failed!');
+}
 
+}
