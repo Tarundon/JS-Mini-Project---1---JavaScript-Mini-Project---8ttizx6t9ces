d2e3d21d77e3a63458a883f9d9eadb00407097b8
Tarundon <tdongre243@gamil.com>
2022-07-02 01:30:09 +0530

    second commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ function validate(){
     let userid = document.getElementById("email").value
     let password = document.getElementById("password").value
     
-    let user_records=new Array();
+    let user_records = new Array();
    user_records=JSON.parse(localStorage.getItem("users"))?JSON.parse(localStorage.getItem("users")):[]
    if(user_records.some((v)=>{return v.email==userid && v.password==password}))
   {
